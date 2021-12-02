b6c8ba6dc15bded84469843915f97cf705f36406
julian-heng <julianhengwl@gmail.com>
2021-12-02 17:49:17 +0800

    Update script

--- 01/day01.py
+++ 01/day01.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
+import sys
 
-def main():
-    with open("input") as f:
+
+def main(fname):
+    with open(fname) as f:
         l = list(map(int, f.read().splitlines()))
         ll = list(map(sum, zip(l, l[1::], l[2::])))
     print(len(solve(l)))
@@ -14,4 +16,4 @@ def solve(l):
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1] if len(sys.argv) > 1 else "input")
--- dayXX.sh
+++ dayXX.sh
@@ -4,14 +4,19 @@
 main() {
     printf -v day "%0.2d" "${1}"
     src_filename="${day}/day${day}.py"
+    sample_input_filename="${day}/sample"
+    input_filename="${day}/input"
 
     mkdir -p "$1"
     cat > "${src_filename}" <<EOF
 #!/usr/bin/env python3
 
+import sys
 
-def main():
-    pass
+
+def main(fname):
+    with open(fname) as f:
+        pass
 
 
 def solve1():
@@ -23,11 +28,13 @@ def solve2():
 
 
 if __name__ == "__main__":
-    main()
-
+    main(sys.argv[1] if len(sys.argv) > 1 else "input")
 EOF
 
     chmod +x "${src_filename}"
+
+    touch "${sample_input_filename}"
+    touch "${input_filename}"
 }
 
 main "$@"
